eb54064bdde7eaa7028b06f54fc33f97fcc39815
Hugo Hu <46556200+0HugoHu@users.noreply.github.com>
2023-02-16 17:17:39 -0500

    Delete accounts directory

--- accounts/views.py
+++
@@ -1,9 +0,0 @@
-# accounts/views.py
-from accounts.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
-
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
